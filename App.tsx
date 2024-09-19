e5afce482011407b427b900a6c33680bd8a51b1d
Tiago Fernandes <tiagoferprojects@gmail.com>
2024-09-18 21:23:45 -0300

    Styled Components Setup

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { NunitoSans_400Regular, NunitoSans_700Bold, useFonts } from '@expo-google-fonts/nunito-sans';
 import { ActivityIndicator, StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -7,17 +9,17 @@ export default function App() {
     NunitoSans_700Bold,
   })
 
-  if(fontsLoaded){
+  if(!fontsLoaded){
     return <ActivityIndicator />
   }
 
   return (
-    <>
+    <ThemeProvider theme={theme} >
       <StatusBar
         backgroundColor="transparent"
         barStyle="dark-content"
         translucent
       />
-    </>
+    </ThemeProvider>
   );
 }
