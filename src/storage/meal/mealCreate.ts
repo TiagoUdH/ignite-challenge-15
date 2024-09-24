148518bb53316e28adce491ff513be1e53519230
Tiago Fernandes <tiagoferprojects@gmail.com>
2024-09-24 08:55:56 -0300

    Implementing meal create function

---
+++ src/storage/meal/mealCreate.ts
@@ -0,0 +1,39 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { sectionsGetAll } from "@storage/sections/sectionsGetAll";
+import { SECTIONS_COLLECTION } from "@storage/storageConfig";
+import { MealDTO } from "./MealDTO";
+
+export async function mealCreate(title: string, meal: MealDTO) {
+  const storedSections = await sectionsGetAll()
+
+  const titleFormatted = title.replace("/", ".")
+
+  const titleExists = storedSections.find(section => section.title === titleFormatted)
+
+  if(titleExists){
+    storedSections.map(section => {
+      if (section.title === titleFormatted){
+        return {
+          ...section,
+          data: [meal, ...section.data]
+        }
+      }
+
+      return section
+    })
+
+    const storage = JSON.stringify(storedSections)
+
+    await AsyncStorage.setItem(SECTIONS_COLLECTION, storage)
+  }
+  else {
+    const item = {
+      title: titleFormatted,
+      data: [meal]
+    }
+
+    const storage = JSON.stringify([item, ...storedSections])
+
+    await AsyncStorage.setItem(SECTIONS_COLLECTION, storage)
+  }
+}
