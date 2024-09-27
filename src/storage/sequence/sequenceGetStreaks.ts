2ee15a2a177c86b991a721c33feb8e7815ed4286
Tiago Fernandes <tiagoferprojects@gmail.com>
2024-09-26 22:01:53 -0300

    Implementing Sequence get streaks function

---
+++ src/storage/sequence/sequenceGetStreaks.ts
@@ -0,0 +1,38 @@
+import { sequenceGet } from "./sequenceGet";
+
+type StreaksAccumulator = {
+  currentStreak: number;
+  bestStreak: number;
+};
+
+export async function sequenceGetStreaks(){
+  try{
+    const storedSequence = await sequenceGet()
+
+    const streaks = storedSequence.reduce<StreaksAccumulator>((acc, term) => {
+      if(term.inDiet){
+        acc.currentStreak++;
+
+        if (acc.currentStreak > acc.bestStreak){
+          acc.bestStreak = acc.currentStreak;
+        }
+      }
+      else {
+        acc.currentStreak = 0;
+      }
+
+      return acc
+    }, {
+      currentStreak: 0,
+      bestStreak: 0,
+    })
+
+    return {
+      currentStreak: streaks.currentStreak,
+      bestStreak: streaks.bestStreak,
+    }
+  }
+  catch(error){
+    throw error
+  }
+}
