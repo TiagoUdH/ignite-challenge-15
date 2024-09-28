05b2e4747d248b9ad8cf320da5c8dfb87cd9dae4
Tiago Fernandes <tiagoferprojects@gmail.com>
2024-09-27 23:37:14 -0300

    Implementing delete meal function

---
+++ src/storage/meal/mealDelete.ts
@@ -0,0 +1,28 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { generalDataAfterMealDelete } from "@storage/general/generalDataAfterMealDelete"
+import { sectionsGetAll } from "@storage/sections/sectionsGetAll"
+import { sequenceAfterDeleteMeal } from "@storage/sequence/sequenceAfterDeleteMeal"
+import { SECTIONS_COLLECTION } from "@storage/storageConfig"
+
+export async function mealGelete(id: string, inDiet: boolean) {
+  try{
+    const sectionsStored = await sectionsGetAll()
+
+    const storage = sectionsStored.map(item => {
+      const updatedData= item.data.filter(meal => meal.id !== id)
+
+      return {
+        ...item,
+        data: updatedData,
+      }    
+    }).filter(item => item.data.length > 0)
+
+    await AsyncStorage.setItem(SECTIONS_COLLECTION, JSON.stringify(storage))
+
+    await sequenceAfterDeleteMeal(id)
+    await generalDataAfterMealDelete(inDiet)
+  }
+  catch(error){
+    throw error
+  }
+}
